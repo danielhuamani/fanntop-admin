5562f63289199ea0485b720464eb599b3144409a
danielhuamani <danielhuamani15@gmail.com>
2017-12-22 06:55:29 -0500

    initial proyecto

---
+++ src/pages/security/login.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="ui middle aligned center aligned grid grid-login">
+        <div class="column">
+            <h2 class="ui header">
+              <img src="../../assets/img/fanntop.svg" class="image">
+              <div class="content">
+                Ingresar
+              </div>
+            </h2>
+            <form class="ui large form ui form error" method="POST" action=".">
+
+                <div class="ui error message">
+
+
+
+                </div>
+
+              <div class="ui stacked segment">
+                <div class="field">
+                  <div class="ui left icon input">
+                    <i class="user icon"></i>
+                  </div>
+                </div>
+                <div class="field">
+                  <div class="ui left icon input">
+                    <i class="lock icon"></i>
+                  </div>
+                </div>
+                <button class="ui fluid large  blue  button">Login</button>
+              </div>
+
+              <div class="ui error message"></div>
+
+            </form>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
