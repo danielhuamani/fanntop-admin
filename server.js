ba2fe54e3f6cf152786a9d6d6dc4226afda4e72c
danielhuamani <danielhuamani15@gmail.com>
2017-12-26 22:27:14 -0500

    modificando server js

--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
-
+var router = express.Router();
 app = express();
 app.use(serveStatic(`${__dirname}/dist`));
+app.get('*',function(req,res){
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
 
 var port = process.env.PORT || 3000;
 app.listen(port);
