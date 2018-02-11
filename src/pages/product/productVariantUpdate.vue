f23ed2c00447c98e8ccabf325ca71deda6e52b9f
danielhuamani <danielhuamani15@gmail.com>
2018-02-11 08:01:33 -0500

    add images

--- src/pages/product/productVariantUpdate.vue
+++ src/pages/product/productVariantUpdate.vue
@@ -18,16 +18,21 @@
             <div class="col-12 material content">
               <h5 class="material__title">Galeria Imagenes</h5>
               <div class="row">
-
                 <div class="col-12">
                   <div class="content__image">
                     <label class="custom-file content__file" id="file_banner">
                     <input type="file" id="file_banner" class="custom-file-input content__file__input"  @change="fileUpload($event, 'banner')">
                     <span class="custom-file-control content__file__control">
-                      <img :src="fileImageBanner" alt="" width="225" height="225">
+                      <img :src="fileImage" alt="" width="225" height="225">
                     </span>
                     </label>
                   </div>
+                </div>
+
+              </div>
+              <div class="row">
+                <div class="col-4" v-for="images in galeryImages">
+                  <img :src="images.image" alt="" width="225" height="225">
                 </div>
 
               </div>
@@ -110,16 +115,33 @@
           maxFilesize: 0.5,
           method: 'post',
           headers: { 'My-Awesome-Header': 'header value' }
-        }
+        },
+        galeryImages: [],
+        fileImage: '',
+        formData: new FormData()
       }
     },
     components: {
       Dropzone
     },
+
     created () {
       this.getProductVariant()
+      this.getGaleryImages()
     },
     methods: {
+      getGaleryImages () {
+        const self = this
+        this.axios({
+          method: 'get',
+          url: '/product/product-galery-image/',
+          params: {
+            'product': self.$route.params.id_variant_update
+          }
+        }).then(response => {
+          self.galeryImages = response.data
+        })
+      },
       getProductVariant () {
         var self = this
         this.axios({
@@ -131,6 +153,46 @@
       },
       showSuccess (file) {
         console.log('A file was successfully uploaded', file)
+      },
+      fileUpload (e, typeImage) {
+        var files = e.target.files || e.dataTransfer.files
+        if (!files.length) {
+          return
+        }
+        // console.log(files[0])
+        // var x = files[0]
+        return this.createImage(files[0], typeImage)
+      // return new Buffer(x)
+      },
+      createImage (file, typeImage) {
+      // var image = new Image()
+        var reader = new FileReader()
+        var vm = this
+
+        reader.onload = (e) => {
+          vm.fileImage = reader.result
+        }
+        reader.readAsDataURL(file)
+        this.formData.append('image', file)
+        this.formData.append('product_class', this.$route.params.id)
+        this.formData.append('product', this.$route.params.id_variant_update)
+        this.saveInfluencer()
+      },
+      saveInfluencer () {
+        var self = this
+        this.axios({
+          method: 'post',
+          url: '/product/product-galery-image/',
+          data: self.formData
+        }).then(response => {
+          this.$emit('alert', 'success', {'Se guardo correctamente': []})
+        }).catch(error => {
+          let status = ''
+          if (error.response.status >= 400 && error.response.status < 500) {
+            status = 'danger'
+          }
+          this.$emit('alert', status, error.response.data)
+        })
       }
     }
   }
